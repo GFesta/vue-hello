5d083e7bcbb6a576c5825725ef371233bc32c0bb
Giuseppe <festagiuseppe89@gmail.com>
2021-03-18 16:22:42 +0100

    avendo tempo ho provato una funzione Vue

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ var app = new Vue({
     el: '#root',
     data: {
         message: 'Hello, Welcome in MSI',
-        image: 'img/msi_1.jfif' //Bonus 1: img aggiunta
+        image: 'img/msi_1.jfif', //Bonus 1: img aggiunta
+        products: 'Prodotti'
     },
+
+
+//ho provato ad aggiungere altre foto usando una funzione Vue
+    methods: {
+        changeImg: function() {
+            if (this.image === 'img/msi_1.jfif'){
+                this.image = 'img/msi_2.jfif'
+            } else if (this.image === 'img/msi_2.jfif'){
+            this.image = 'img/msi_3.jfif';
+            this.products = 'Prodotti!'
+            } else if (this.image === 'img/img/msi_3.jfif'){
+            this.image = 'img/msi_1.jfif';
+            this.products = 'Prodotti!'
+            }
+        }
+    }
 });
