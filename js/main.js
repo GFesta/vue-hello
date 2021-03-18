35e8ff22cbb082f198d84d44cb12c64e5660d9b4
Giuseppe <festagiuseppe89@gmail.com>
2021-03-18 16:11:18 +0100

    esercizio completato + bonus fatto

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+//Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+var app = new Vue({
+    el: '#root',
+    data: {
+        message: 'Hello, Welcome in MSI',
+        image: 'img/msi_1.jfif' //Bonus 1: img aggiunta
+    },
+});
